dbb850d3d5dde9a03dba0a125682d22e4fb88e5f
Dino Oliva <dpo@google.com>
2016-09-27 09:55:04 -0700

    Fixes javadoc.

--- core/java/com/google/census/CensusContextFactory.java
+++ core/java/com/google/census/CensusContextFactory.java
@@ -21,9 +21,10 @@ import javax.annotation.Nullable;
  */
 public abstract class CensusContextFactory {
   /**
-   * Creates a {@link CensusContext} from the given on-the-wire encoded representation. Should be
-   * the inverse of {@link CensusContext#serialize()}. The serialized representation should be
-   * based on the {@link CensusContext} protobuf representation.
+   * Creates a {@link CensusContext} from the given on-the-wire encoded representation.
+   *
+   * <p>Should be the inverse of {@link CensusContext#serialize()}. The serialized representation
+   * should be based on the {@link CensusContext} protobuf representation.
    *
    * @param buffer on-the-wire representation of a {@link CensusContext}
    * @return a {@link CensusContext} deserialized from {@code buffer}
