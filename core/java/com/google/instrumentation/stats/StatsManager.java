da8cd758ea18927bbd7292af8e6acd2851d0e7b6
Dino Oliva <dpo@google.com>
2016-12-06 09:14:19 -0800

    Adds basic APIs for collecting and retrieving stats via the pull model.

---
+++ core/java/com/google/instrumentation/stats/StatsManager.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2016, Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.instrumentation.stats;
+
+import java.util.List;
+
+/**
+ * Provides facillities to register {@link ViewDescriptor}s for collecting stats and retrieving
+ * stats data as a {@link View}.
+ */
+public abstract class StatsManager {
+  /**
+   * Pull model for stats. Registers a {@link @View} that will collect data to be accessed via
+   * {@link getView(ViewDescriptor)}.
+   */
+  public abstract void registerView(ViewDescriptor view);
+
+  /**
+   * Returns the current stats data, {@link View}, associated with the given {@link ViewDescriptor}.
+   */
+  public abstract View getView(ViewDescriptor viewDescriptor);
+}
