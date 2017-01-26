32e07098a1ec512540dacf422b111cc3a76fb0e1
Bogdan Drutu <bdrutu@google.com>
2017-01-26 15:30:08 -0800

    Add more comments and example of use.

--- core/java/com/google/instrumentation/common/NonThrowingCloseable.java
+++ core/java/com/google/instrumentation/common/NonThrowingCloseable.java
@@ -4,6 +4,17 @@ import java.io.Closeable;
 
 /**
  * An {@link Closeable} which cannot throw a checked exception.
+ *
+ * <p>This is useful because such a reversion otherwise requires the caller to catch the
+ * (impossible) Exception in the try-with-resources.
+ *
+ * <p>Example of usage:
+ *
+ * <pre>
+ *   try (NonThrowingAutoCloseable ctx = tryEnter()) {
+ *     ...
+ *   }
+ * </pre>
  */
 public interface NonThrowingCloseable extends Closeable {
   @Override
