59a1ef623775d64925e65b693c44e8ea1de7b51c
Kristen Kozak <sebright@google.com>
2017-02-02 10:56:56 -0800

    Allows PRs to not branch from the base branch.
    Forcing PRs to branch from the base branch would interfere with creating
    dependent pull requests.

--- check-git-history.py
+++ check-git-history.py
@@ -7,10 +7,9 @@ def main(argv):
     # Only check the history if the build is running on a pull request. Travis
     # always merges pull requests into the base branch before running the build.
     if is_merged_pull_request():
-      # These functions assume that HEAD^1 is the base branch and HEAD^2 is the
+      # This function assumes that HEAD^1 is the base branch and HEAD^2 is the
       # pull request.
       exit_if_pull_request_has_merge_commits()
-      exit_if_pull_request_does_not_start_from_base_branch()
       print 'Checked pull request history.'
     else:
       print 'Skipped history check.'
@@ -37,44 +36,6 @@ def exit_if_pull_request_has_merge_commits():
       print 'Pull request contains a merge commit:'
       print_history()
       sys.exit(1)
-
-def exit_if_pull_request_does_not_start_from_base_branch():
-  '''Exits with an error if the pull request is not branched from a commit on
-     the base branch.'''
-  start = get_commit(list_pull_request_commits()[-1] + '^')
-  if not start in list_commits('HEAD^1'):
-    print 'Pull request does not start from the base branch:'
-    print_history()
-    sys.exit(1)
-
-def list_pull_request_commits():
-  '''Returns a list of all commit hashes that are contained in the pull
-     request but not the base branch.'''
-  return get_log_commits('git log --format="%H" HEAD^1..HEAD^2')
-
-def list_commits(branch):
-  '''Returns a list of all commit hashes on the given branch, following only
-     the first parent.'''
-  return get_log_commits('git log --first-parent --format="%H" ' + branch)
-
-def get_log_commits(git_log_command):
-  commits = []
-  for line in os.popen(git_log_command):
-    commit = line.strip()
-    assert_commit(commit)
-    commits.append(commit)
-  return commits
-
-def get_commit(revision):
-  git_command = 'git show --no-patch --format="%H" ' + revision
-  commit = read_process(git_command).strip()
-  assert_commit(commit)
-  return commit
-
-def assert_commit(commit):
-  '''Assert that the string has the format of a git commit hash.'''
-  assert set(commit) <= set('abcdef0123456789'), commit
-  assert len(commit) == 40, commit
 
 def print_history():
   os.system('git log HEAD^1 HEAD^2 -30 --graph --oneline --decorate')
