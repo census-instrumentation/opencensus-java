e68ff45a533674f9906a724887c0d3f799617599
Dino Oliva <dpo@google.com>
2017-02-06 12:20:02 -0800

    Adds interval stats views.

--- core/java/com/google/instrumentation/stats/RpcConstants.java
+++ core/java/com/google/instrumentation/stats/RpcConstants.java
@@ -13,11 +13,14 @@
 
 package com.google.instrumentation.stats;
 
+import com.google.instrumentation.common.Duration;
 import com.google.instrumentation.stats.MeasurementDescriptor.BasicUnit;
 import com.google.instrumentation.stats.MeasurementDescriptor.MeasurementUnit;
 import com.google.instrumentation.stats.ViewDescriptor.DistributionViewDescriptor;
+import com.google.instrumentation.stats.ViewDescriptor.IntervalViewDescriptor;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -116,15 +119,15 @@ public final class RpcConstants {
           MeasurementUnit.create(6, bytes));
 
   // Common histogram bucket boundaries for bytes received/sets DistributionViewDescriptors.
-  static final List<Double> RPC_BYTES_BUCKET_BOUNDARIES = Arrays.asList(
-        0.0, 1024.0, 2048.0, 4096.0, 16384.0, 65536.0, 262144.0, 1048576.0, 4194304.0, 16777216.0,
-        67108864.0, 268435456.0, 1073741824.0, 4294967296.0);
+  static final List<Double> RPC_BYTES_BUCKET_BOUNDARIES = Collections.unmodifiableList(
+      Arrays.asList(0.0, 1024.0, 2048.0, 4096.0, 16384.0, 65536.0, 262144.0, 1048576.0, 4194304.0,
+          16777216.0, 67108864.0, 268435456.0, 1073741824.0, 4294967296.0));
 
   // Common histogram bucket boundaries for latency and elapsed-time DistributionViewDescriptors.
-  static final List<Double> RPC_MILLIS_BUCKET_BOUNDARIES = Arrays.asList(
-        0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0, 13.0, 16.0, 20.0, 25.0, 30.0, 40.0, 50.0,
-        65.0, 80.0, 100.0, 130.0, 160.0, 200.0, 250.0, 300.0, 400.0, 500.0, 650.0, 800.0, 1000.0,
-        2000.0, 5000.0, 10000.0, 20000.0, 50000.0, 100000.0);
+  static final List<Double> RPC_MILLIS_BUCKET_BOUNDARIES = Collections.unmodifiableList(
+      Arrays.asList(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0, 13.0, 16.0, 20.0, 25.0, 30.0,
+          40.0, 50.0, 65.0, 80.0, 100.0, 130.0, 160.0, 200.0, 250.0, 300.0, 400.0, 500.0, 650.0,
+          800.0, 1000.0, 2000.0, 5000.0, 10000.0, 20000.0, 50000.0, 100000.0));
 
   /**
    * Census defined rpc client {@link ViewDescriptor}s.
@@ -216,6 +219,128 @@ public final class RpcConstants {
           "Uncompressed Request MB",
           RPC_SERVER_UNCOMPRESSED_RESPONSE_BYTES,
           DistributionAggregationDescriptor.create(RPC_BYTES_BUCKET_BOUNDARIES),
+          Arrays.asList(RPC_SERVER_METHOD));
+
+  // Interval Stats
+  static final Duration MINUTE = Duration.create(60, 0);
+  static final Duration HOUR = Duration.create(60 * 60, 0);
+
+  /**
+   * Census defined rpc client {@link IntervalViewDescriptor}s.
+   */
+  public static final IntervalViewDescriptor RPC_CLIENT_ROUNDTRIP_LATENCY_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc client roundtrip latency",
+          "Latency in msecs",
+          RPC_CLIENT_ROUNDTRIP_LATENCY,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_CLIENT_METHOD));
+
+  public static final IntervalViewDescriptor RPC_CLIENT_REQUEST_BYTES_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc client request_bytes",
+          "Request MB",
+          RPC_CLIENT_REQUEST_BYTES,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_CLIENT_METHOD));
+
+  public static final IntervalViewDescriptor RPC_CLIENT_RESPONSE_BYTES_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc client response_bytes",
+          "Response MB",
+          RPC_CLIENT_RESPONSE_BYTES,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_CLIENT_METHOD));
+
+  public static final IntervalViewDescriptor RPC_CLIENT_ERROR_COUNT_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc client error count",
+          "RPC Errors",
+          RPC_CLIENT_ERROR_COUNT,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_CLIENT_METHOD));
+
+  public static final IntervalViewDescriptor RPC_CLIENT_UNCOMPRESSED_REQUEST_BYTES_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc client uncompressed_request_bytes",
+          "Uncompressed Request MB",
+          RPC_CLIENT_UNCOMPRESSED_REQUEST_BYTES,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_CLIENT_METHOD));
+
+  public static final IntervalViewDescriptor RPC_CLIENT_UNCOMPRESSED_RESPONSE_BYTES_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc client uncompressed_response_bytes",
+          "Uncompressed Response MB",
+          RPC_CLIENT_RESPONSE_BYTES,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_CLIENT_METHOD));
+
+  public static final IntervalViewDescriptor RPC_CLIENT_SERVER_ELAPSED_TIME_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc client server_elapsed_time",
+          "Server elapsed time in msecs",
+          RPC_CLIENT_SERVER_ELAPSED_TIME,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_CLIENT_METHOD));
+
+  /**
+   * Census defined rpc server {@link IntervalViewDescriptor}s.
+   */
+  public static final IntervalViewDescriptor RPC_SERVER_SERVER_LATENCY_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc server roundtrip latency",
+          "Latency in msecs",
+          RPC_SERVER_SERVER_LATENCY,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_SERVER_METHOD));
+
+  public static final IntervalViewDescriptor RPC_SERVER_REQUEST_BYTES_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc server request_bytes",
+          "Request MB",
+          RPC_SERVER_REQUEST_BYTES,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_SERVER_METHOD));
+
+  public static final IntervalViewDescriptor RPC_SERVER_RESPONSE_BYTES_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc server response_bytes",
+          "Response MB",
+          RPC_SERVER_RESPONSE_BYTES,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_SERVER_METHOD));
+
+  public static final IntervalViewDescriptor RPC_SERVER_ERROR_COUNT_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc server error count",
+          "RPC Errors",
+          RPC_SERVER_ERROR_COUNT,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_SERVER_METHOD));
+
+  public static final IntervalViewDescriptor RPC_SERVER_UNCOMPRESSED_REQUEST_BYTES_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc server uncompressed_request_bytes",
+          "Uncompressed Request MB",
+          RPC_SERVER_UNCOMPRESSED_REQUEST_BYTES,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_SERVER_METHOD));
+
+  public static final IntervalViewDescriptor RPC_SERVER_UNCOMPRESSED_RESPONSE_BYTES_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc server uncompressed_response_bytes",
+          "Uncompressed Response MB",
+          RPC_SERVER_RESPONSE_BYTES,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
+          Arrays.asList(RPC_SERVER_METHOD));
+
+  public static final IntervalViewDescriptor RPC_SERVER_SERVER_ELAPSED_TIME_INTERVAL_VIEW =
+      IntervalViewDescriptor.create(
+          "rpc server server_elapsed_time",
+          "Server elapsed time in msecs",
+          RPC_SERVER_SERVER_ELAPSED_TIME,
+          IntervalAggregationDescriptor.create(Arrays.asList(MINUTE, HOUR)),
           Arrays.asList(RPC_SERVER_METHOD));
 
   // Visible for testing
