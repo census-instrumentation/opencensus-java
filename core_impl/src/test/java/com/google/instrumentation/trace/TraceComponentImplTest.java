6a9d8bb5ddd4b6b272f46b6e460c52dcd90ff0d2
Ian Sturdy <sturdyi12@mail.wlu.edu>
2017-04-20 18:10:37 -0700

    Add required imports too.

--- core_impl/src/test/java/com/google/instrumentation/trace/TraceComponentImplTest.java
+++ core_impl/src/test/java/com/google/instrumentation/trace/TraceComponentImplTest.java
@@ -3,6 +3,8 @@ package com.google.instrumentation.trace;
 import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /** Unit tests for {@link TraceComponentImpl}. */
 @RunWith(JUnit4.class)
