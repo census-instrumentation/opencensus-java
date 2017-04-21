937906b5f874b5436c42f77f6a41dbb59fd6c735
Ian Sturdy <sturdyi12@mail.wlu.edu>
2017-04-20 18:17:32 -0700

    Add a missing RunWith annotation. (#246)

--- core_impl/src/test/java/com/google/instrumentation/trace/TraceComponentImplTest.java
+++ core_impl/src/test/java/com/google/instrumentation/trace/TraceComponentImplTest.java
@@ -3,8 +3,11 @@ package com.google.instrumentation.trace;
 import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /** Unit tests for {@link TraceComponentImpl}. */
+@RunWith(JUnit4.class)
 public class TraceComponentImplTest {
   @Test
   public void implementationOfTracer() {
