5c2dfedd45574b45e2c72c718feb428b7376d5e4
Kristen Kozak <sebright@google.com>
2017-04-25 16:04:56 -0700

    Update tags API to use a builder.

--- core/src/main/java/com/google/instrumentation/tags/TagSet.java
+++ core/src/main/java/com/google/instrumentation/tags/TagSet.java
@@ -27,7 +27,7 @@ public abstract class TagSet {
   TagSet() {}
 
   static TagSet createInternal(Map<TagKey<?>, Object> tags) {
-    return new AutoValue_TagSet(new HashMap<TagKey<?>, Object>(tags));
+    return new AutoValue_TagSet(tags);
   }
 
   abstract Map<TagKey<?>, Object> getTags();
@@ -42,39 +42,168 @@ public abstract class TagSet {
   }
 
   /**
-   * Creates a new {@code TagSet} by applying the given {@code TagChange}s.
+   * Returns an empty builder.
    *
-   * @param changes {@code TagChange}s used to create the new {@code TagSet}.
-   * @return the updated {@code TagSet}.
+   * @return an empty builder.
    */
-  public TagSet newContext(TagChange... changes) {
-    Map<TagKey<?>, Object> tags = new HashMap<TagKey<?>, Object>(getTags());
-    for (TagChange tc : changes) {
-      applyChange(tags, tc);
-    }
-    return createInternal(tags);
+  public static Builder builder() {
+    return new Builder();
   }
 
-  private static void applyChange(Map<TagKey<?>, Object> tags, TagChange tc) {
-    switch (tc.getOp()) {
-      case INSERT:
-        if (!tags.containsKey(tc.getKey())) {
-          tags.put(tc.getKey(), tc.getValue());
-        }
-        return;
-      case SET:
-        tags.put(tc.getKey(), tc.getValue());
-        return;
-      case UPDATE:
-        if (tags.containsKey(tc.getKey())) {
-          tags.put(tc.getKey(), tc.getValue());
-        }
-        return;
-      case CLEAR:
-        tags.remove(tc.getKey());
-        return;
-      case NOOP:
-        return;
+  /**
+   * Returns a builder based on this {@code TagSet}.
+   *
+   * @return a builder based on this {@code TagSet}.
+   */
+  public Builder toBuilder() {
+    return new Builder(getTags());
+  }
+
+  /**
+   * Builder for the {@link TagSet} class.
+   */
+  public static final class Builder {
+    private final Map<TagKey<?>, Object> tags;
+
+    private Builder(Map<TagKey<?>, Object> tags) {
+      this.tags = new HashMap<TagKey<?>, Object>(tags);
+    }
+
+    private Builder() {
+      tags = new HashMap<TagKey<?>, Object>();
+    }
+
+    /**
+     * Adds the key/value pair if the key is not present.
+     *
+     * @param key the key to look up.
+     * @param value the value to insert for the given key.
+     * @return this
+     */
+    public Builder insert(TagKey<String> key, String value) {
+      return insertInternal(key, value);
+    }
+
+    /**
+     * Adds the key/value pair if the key is not present.
+     *
+     * @param key the key to look up.
+     * @param value the value to insert for the given key.
+     * @return this
+     */
+    public Builder insert(TagKey<Long> key, long value) {
+      return insertInternal(key, value);
+    }
+
+    /**
+     * Adds the key/value pair if the key is not present.
+     *
+     * @param key the key to look up.
+     * @param value the value to insert for the given key.
+     * @return this
+     */
+    public Builder insert(TagKey<Boolean> key, boolean value) {
+      return insertInternal(key, value);
+    }
+
+    private <TagValueT> Builder insertInternal(TagKey<TagValueT> key, TagValueT value) {
+      if (!tags.containsKey(key)) {
+        tags.put(key, value);
+      }
+      return this;
+    }
+
+    /**
+     * Adds the key/value pair regardless of whether the key is present.
+     *
+     * @param key the key to look up.
+     * @param value the value to set for the given key.
+     * @return this
+     */
+    public Builder set(TagKey<String> key, String value) {
+      return setInternal(key, value);
+    }
+
+    /**
+     * Adds the key/value pair regardless of whether the key is present.
+     *
+     * @param key the key to look up.
+     * @param value the value to set for the given key.
+     * @return this
+     */
+    public Builder set(TagKey<Long> key, long value) {
+      return setInternal(key, value);
+    }
+
+    /**
+     * Adds the key/value pair regardless of whether the key is present.
+     *
+     * @param key the key to look up.
+     * @param value the value to set for the given key.
+     * @return this
+     */
+    public Builder set(TagKey<Boolean> key, boolean value) {
+      return setInternal(key, value);
+    }
+
+    private <TagValueT> Builder setInternal(TagKey<TagValueT> key, TagValueT value) {
+      tags.put(key, value);
+      return this;
+    }
+
+    /**
+     * Adds the key/value pair only if the key is already present.
+     *
+     * @param key the key to look up.
+     * @param value the value to update for the given key.
+     * @return this
+     */
+    public Builder update(TagKey<String> key, String value) {
+      return updateInternal(key, value);
+    }
+
+    /**
+     * Adds the key/value pair only if the key is already present.
+     *
+     * @param key the key to look up.
+     * @param value the value to update for the given key.
+     * @return this
+     */
+    public Builder update(TagKey<Long> key, long value) {
+      return updateInternal(key, value);
+    }
+
+    /**
+     * Adds the key/value pair only if the key is already present.
+     *
+     * @param key the key to look up.
+     * @param value the value to update for the given key.
+     * @return this
+     */
+    public Builder update(TagKey<Boolean> key, boolean value) {
+      return updateInternal(key, value);
+    }
+
+    private <TagValueT> Builder updateInternal(TagKey<TagValueT> key, TagValueT value) {
+      if (tags.containsKey(key)) {
+        tags.put(key, value);
+      }
+      return this;
+    }
+
+    /**
+     * Removes the key if it exists.
+     *
+     * @param key the key to look up.
+     * @return this
+     */
+    public Builder clear(TagKey<?> key) {
+      tags.remove(key);
+      return this;
+    }
+
+    public TagSet build() {
+      return createInternal(new HashMap<TagKey<?>, Object>(tags));
     }
   }
 }
--- core/src/test/java/com/google/instrumentation/tags/TagSetTest.java
+++ core/src/test/java/com/google/instrumentation/tags/TagSetTest.java
@@ -37,56 +37,38 @@ public class TagSetTest {
 
   @Test
   public void applyTagChangesInOrder() {
-    assertThat(
-            TagSet.empty()
-                .newContext(
-                    TagChange.create(KS1, TagOp.SET, "v1"), TagChange.create(KS1, TagOp.SET, "v2"))
-                .getTags())
+    assertThat(TagSet.builder().set(KS1, "v1").set(KS1, "v2").build().getTags())
         .containsExactly(KS1, "v2");
   }
 
   @Test
   public void testInsert() {
     TagSet ctx = singletonTagSet(KS1, "v1");
-    assertThat(ctx.newContext(TagChange.create(KS1, TagOp.INSERT, "v2")).getTags())
-        .containsExactly(KS1, "v1");
-    assertThat(ctx.newContext(TagChange.create(KS2, TagOp.INSERT, "v2")).getTags())
+    assertThat(ctx.toBuilder().insert(KS1, "v2").build().getTags()).containsExactly(KS1, "v1");
+    assertThat(ctx.toBuilder().insert(KS2, "v2").build().getTags())
         .containsExactly(KS1, "v1", KS2, "v2");
   }
 
   @Test
   public void testSet() {
     TagSet ctx = singletonTagSet(KS1, "v1");
-    assertThat(ctx.newContext(TagChange.create(KS1, TagOp.SET, "v2")).getTags())
-        .containsExactly(KS1, "v2");
-    assertThat(ctx.newContext(TagChange.create(KS2, TagOp.SET, "v2")).getTags())
+    assertThat(ctx.toBuilder().set(KS1, "v2").build().getTags()).containsExactly(KS1, "v2");
+    assertThat(ctx.toBuilder().set(KS2, "v2").build().getTags())
         .containsExactly(KS1, "v1", KS2, "v2");
   }
 
   @Test
   public void testUpdate() {
     TagSet ctx = singletonTagSet(KS1, "v1");
-    assertThat(ctx.newContext(TagChange.create(KS1, TagOp.UPDATE, "v2")).getTags())
-        .containsExactly(KS1, "v2");
-    assertThat(ctx.newContext(TagChange.create(KS2, TagOp.UPDATE, "v2")).getTags())
-        .containsExactly(KS1, "v1");
+    assertThat(ctx.toBuilder().update(KS1, "v2").build().getTags()).containsExactly(KS1, "v2");
+    assertThat(ctx.toBuilder().update(KS2, "v2").build().getTags()).containsExactly(KS1, "v1");
   }
 
   @Test
   public void testClear() {
     TagSet ctx = singletonTagSet(KS1, "v1");
-    assertThat(ctx.newContext(TagChange.create(KS1, TagOp.CLEAR, "v2")).getTags()).isEmpty();
-    assertThat(ctx.newContext(TagChange.create(KS2, TagOp.CLEAR, "v2")).getTags())
-        .containsExactly(KS1, "v1");
-  }
-
-  @Test
-  public void testNoop() {
-    TagSet ctx = singletonTagSet(KS1, "v1");
-    assertThat(ctx.newContext(TagChange.create(KS1, TagOp.NOOP, "v2")).getTags())
-        .containsExactly(KS1, "v1");
-    assertThat(ctx.newContext(TagChange.create(KS2, TagOp.NOOP, "v2")).getTags())
-        .containsExactly(KS1, "v1");
+    assertThat(ctx.toBuilder().clear(KS1).build().getTags()).isEmpty();
+    assertThat(ctx.toBuilder().clear(KS2).build().getTags()).containsExactly(KS1, "v1");
   }
 
   @Test
@@ -94,22 +76,18 @@ public class TagSetTest {
     new EqualsTester()
         .addEqualityGroup(TagSet.empty(), TagSet.empty())
         .addEqualityGroup(
-            TagSet.empty().newContext(TagChange.create(KS1, TagOp.INSERT, "v1")),
-            TagSet.empty().newContext(TagChange.create(KS1, TagOp.SET, "v1")),
-            TagSet.empty().newContext(
-                TagChange.create(KS1, TagOp.SET, "v1"), TagChange.create(KS1, TagOp.SET, "v1")))
+            TagSet.builder().insert(KS1, "v1").build(),
+            TagSet.builder().set(KS1, "v1").build(),
+            TagSet.builder().set(KS1, "v1").set(KS1, "v1").build())
         .addEqualityGroup(
-            TagSet.empty().newContext(
-                TagChange.create(KB, TagOp.SET, true), TagChange.create(KI, TagOp.SET, 2L)),
-            TagSet.empty().newContext(
-                TagChange.create(KI, TagOp.SET, 2L), TagChange.create(KB, TagOp.SET, true)))
-        .addEqualityGroup(TagSet.empty().newContext(TagChange.create(KS1, TagOp.SET, "v2")))
-        .addEqualityGroup(TagSet.empty().newContext(TagChange.create(KS2, TagOp.SET, "v1")))
+            TagSet.builder().set(KB, true).set(KI, 2L).build(),
+            TagSet.builder().set(KI, 2L).set(KB, true).build())
+        .addEqualityGroup(TagSet.builder().set(KS1, "v2"))
+        .addEqualityGroup(TagSet.builder().set(KS2, "v1"))
         .testEquals();
   }
 
-  private static <TagValueT> TagSet singletonTagSet(
-      TagKey<TagValueT> key, TagValueT value) {
+  private static <TagValueT> TagSet singletonTagSet(TagKey<TagValueT> key, TagValueT value) {
     return TagSet.createInternal(ImmutableMap.<TagKey<?>, Object>of(key, value));
   }
 }
