f27ee518014266152c87b87695964d6dfa54c7ad
Bogdan Drutu <bdrutu@google.com>
2017-06-08 17:38:43 -0700

    Cleanup in config package. (#346)
    * Remove unimplemented method from TraceConfig.
    * Move config implementations to config package.
    * Add tests for NoopTraceConfig.

--- core_impl/src/main/java/io/opencensus/trace/TraceComponentImplBase.java
+++ core_impl/src/main/java/io/opencensus/trace/TraceComponentImplBase.java
@@ -17,6 +17,7 @@ import io.opencensus.common.Clock;
 import io.opencensus.common.EventQueue;
 import io.opencensus.trace.SpanImpl.StartEndHandler;
 import io.opencensus.trace.config.TraceConfig;
+import io.opencensus.trace.config.TraceConfigImpl;
 import io.opencensus.trace.export.ExportComponent;
 import io.opencensus.trace.propagation.PropagationComponent;
 import io.opencensus.trace.propagation.PropagationComponentImpl;
