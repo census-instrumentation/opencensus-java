63d0e298114af0979bd54ffe1489aa800f9de49a
Kristen Kozak <sebright@google.com>
2017-06-27 17:08:43 -0700

    Change RpcConstants.RPC_STATUS to "canonical_status".

--- core/src/main/java/com/google/instrumentation/stats/RpcConstants.java
+++ core/src/main/java/com/google/instrumentation/stats/RpcConstants.java
@@ -28,7 +28,7 @@ import java.util.List;
  */
 public final class RpcConstants {
   // Rpc tag keys.
-  public static final TagKey RPC_STATUS = TagKey.create("OpStatus");
+  public static final TagKey RPC_STATUS = TagKey.create("canonical_status");
   public static final TagKey RPC_CLIENT_METHOD = TagKey.create("method");
   public static final TagKey RPC_SERVER_METHOD = TagKey.create("method");
 
