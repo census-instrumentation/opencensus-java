400e3f07cb9c220300b0944afb8f6d7b3b72dc31
Kristen Kozak <sebright@google.com>
2017-07-11 08:52:40 -0700

    Add io.opencensus.tags.CurrentTagsUtils.
    CurrentTagsUtils is based on CurrentStatsContextUtils.

---
+++ core/src/main/java/io/opencensus/tags/CurrentTagsUtils.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2017, Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.tags;
+
+import io.grpc.Context;
+import io.opencensus.common.NonThrowingCloseable;
+import io.opencensus.tags.unsafe.ContextUtils;
+
+/**
+ * Utility methods for accessing the {@link TagContext} contained in the {@link io.grpc.Context}.
+ */
+final class CurrentTagsUtils {
+
+  private CurrentTagsUtils() {}
+
+  /**
+   * Returns the {@link TagContext} from the current context.
+   *
+   * @return the {@code TagContext} from the current context.
+   */
+  static TagContext getCurrentTagContext() {
+    return ContextUtils.TAG_CONTEXT_KEY.get(Context.current());
+  }
+
+  /**
+   * Enters the scope of code where the given {@link TagContext} is in the current context and
+   * returns an object that represents that scope. The scope is exited when the returned object is
+   * closed.
+   *
+   * @param tags the {@code TagContext} to be set to the current context.
+   * @return an object that defines a scope where the given {@code TagContext} is set to the current
+   *     context.
+   */
+  static NonThrowingCloseable withTagContext(TagContext tags) {
+    return new WithTagContext(tags);
+  }
+
+  private static final class WithTagContext implements NonThrowingCloseable {
+
+    private final Context orig;
+
+    /**
+     * Constructs a new {@link WithTagContext}.
+     *
+     * @param tags the {@code TagContext} to be added to the current {@code Context}.
+     */
+    private WithTagContext(TagContext tags) {
+      orig = Context.current().withValue(ContextUtils.TAG_CONTEXT_KEY, tags).attach();
+    }
+
+    @Override
+    public void close() {
+      Context.current().detach(orig);
+    }
+  }
+}
---
+++ core/src/test/java/io/opencensus/tags/CurrentTagsUtilsTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2017, Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.tags;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import io.grpc.Context;
+import io.opencensus.common.NonThrowingCloseable;
+import io.opencensus.tags.TagKey.TagKeyString;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Unit tests for {@link CurrentTagsUtils}. */
+@RunWith(JUnit4.class)
+public class CurrentTagsUtilsTest {
+
+  private static final TagContext TAG_CONTEXT =
+      TagContext.newBuilder()
+          .set(TagKeyString.create("Key"), TagValueString.create("Value"))
+          .build();
+
+  @Test
+  public void testGetCurrentTagContext_WhenNoContext() {
+    assertThat(CurrentTagsUtils.getCurrentTagContext()).isEqualTo(TagContext.EMPTY);
+  }
+
+  @Test
+  public void testWithTagContext() {
+    assertThat(CurrentTagsUtils.getCurrentTagContext()).isEqualTo(TagContext.EMPTY);
+    NonThrowingCloseable scopedTags = CurrentTagsUtils.withTagContext(TAG_CONTEXT);
+    try {
+      assertThat(CurrentTagsUtils.getCurrentTagContext()).isEqualTo(TAG_CONTEXT);
+    } finally {
+      scopedTags.close();
+    }
+    assertThat(CurrentTagsUtils.getCurrentTagContext()).isEqualTo(TagContext.EMPTY);
+  }
+
+  @Test
+  public void testWithTagContextUsingWrap() {
+    Runnable runnable;
+    NonThrowingCloseable scopedTags = CurrentTagsUtils.withTagContext(TAG_CONTEXT);
+    try {
+      assertThat(CurrentTagsUtils.getCurrentTagContext()).isEqualTo(TAG_CONTEXT);
+      runnable =
+          Context.current()
+              .wrap(
+                  new Runnable() {
+                    @Override
+                    public void run() {
+                      assertThat(CurrentTagsUtils.getCurrentTagContext()).isEqualTo(TAG_CONTEXT);
+                    }
+                  });
+    } finally {
+      scopedTags.close();
+    }
+    assertThat(CurrentTagsUtils.getCurrentTagContext()).isEqualTo(TagContext.EMPTY);
+    // When we run the runnable we will have the TagContext in the current Context.
+    runnable.run();
+  }
+}
