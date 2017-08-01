c60fda7882ae89415a01806cf2f0b2089696fd0d
Kristen Kozak <sebright@google.com>
2017-07-31 19:41:07 -0700

    Explain the TagKey class.

--- core/src/main/java/io/opencensus/tags/TagKey.java
+++ core/src/main/java/io/opencensus/tags/TagKey.java
@@ -29,6 +29,10 @@ import javax.annotation.concurrent.Immutable;
  *
  * <p>Each {@code TagKey} has a {@code String} name. Names have a maximum length of {@link
  * #MAX_LENGTH} and contain only printable ASCII characters.
+ *
+ * <p>{@code TagKey}s are designed to be used as constants. Declaring each key as a constant ensures
+ * that the keys have consistent value types and prevents key names from being validated multiple
+ * times.
  */
 @Immutable
 public abstract class TagKey {
