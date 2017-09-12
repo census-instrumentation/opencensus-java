f0f5c3d73be5d0e116cf2d8345e54ca7e0c92445
Adrian Cole <acole@pivotal.io>
2017-09-12 23:16:58 +0200

    Adds ZipkinExporter
    This adds `ZipkinExporter` which allows you to report traces to Zipkin.
    
    By default, this uses http, but it can also work with Kafka, SQS, etc
    as you can override the `Sender`.

---
+++ exporters/trace_zipkin/src/main/java/io/opencensus/exporter/trace/zipkin/ZipkinExporter.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2017, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.exporter.trace.zipkin;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.opencensus.trace.Tracing;
+import io.opencensus.trace.export.SpanExporter;
+import io.opencensus.trace.export.SpanExporter.Handler;
+import javax.annotation.concurrent.GuardedBy;
+import zipkin2.Span;
+import zipkin2.codec.SpanBytesEncoder;
+import zipkin2.reporter.Sender;
+import zipkin2.reporter.urlconnection.URLConnectionSender;
+
+/**
+ * An OpenCensus span exporter implementation which exports data to Zipkin.
+ *
+ * <p>Example of usage:
+ *
+ * <pre><code>
+ *   public static void main(String[] args) {
+ *     ZipkinExporter.createAndRegister("http://127.0.0.1:9411/api/v2/spans", "myservicename");
+ *     ... // Do work.
+ *   }
+ * </code></pre>
+ */
+public final class ZipkinExporter {
+
+  private static final String REGISTER_NAME = ZipkinExporter.class.getName();
+  private static final Object monitor = new Object();
+
+  @GuardedBy("monitor")
+  private static Handler handler = null;
+
+  private ZipkinExporter() {}
+
+  /**
+   * Creates and registers the Zipkin Trace exporter to the OpenCensus library. Only one Zipkin
+   * exporter can be registered at any point.
+   *
+   * @param v2Url Ex http://127.0.0.1:9411/api/v2/spans
+   * @param serviceName the {@link Span#localServiceName() local service name} of the process.
+   * @throws IllegalStateException if a Zipkin exporter is already registered.
+   */
+  public static void createAndRegister(String v2Url, String serviceName) {
+    createAndRegister(SpanBytesEncoder.JSON_V2, URLConnectionSender.create(v2Url), serviceName);
+  }
+
+  /**
+   * Creates and registers the Zipkin Trace exporter to the OpenCensus library. Only one Zipkin
+   * exporter can be registered at any point.
+   *
+   * @param encoder Usually {@link SpanBytesEncoder#JSON_V2}
+   * @param sender  Often, but not necessarily an http sender. This could be Kafka or SQS.
+   * @param serviceName the {@link Span#localServiceName() local service name} of the process.
+   * @throws IllegalStateException if a Zipkin exporter is already registered.
+   */
+  public static void createAndRegister(SpanBytesEncoder encoder, Sender sender,
+      String serviceName) {
+    synchronized (monitor) {
+      checkState(handler == null, "Zipkin exporter is already registered.");
+      handler = new ZipkinExporterHandler(encoder, sender, serviceName);
+      register(Tracing.getExportComponent().getSpanExporter(), handler);
+    }
+  }
+
+  /**
+   * Registers the {@code ZipkinExporter}.
+   *
+   * @param spanExporter the instance of the {@code SpanExporter} where this service is registered.
+   */
+  @VisibleForTesting
+  static void register(SpanExporter spanExporter, Handler handler) {
+    spanExporter.registerHandler(REGISTER_NAME, handler);
+  }
+
+  /**
+   * Unregisters the Zipkin Trace exporter from the OpenCensus library.
+   *
+   * @throws IllegalStateException if a Zipkin exporter is not registered.
+   */
+  public static void unregister() {
+    synchronized (monitor) {
+      checkState(handler != null, "Zipkin exporter is not registered.");
+      unregister(Tracing.getExportComponent().getSpanExporter());
+      handler = null;
+    }
+  }
+
+  /**
+   * Unregisters the {@code ZipkinExporter}.
+   *
+   * @param spanExporter the instance of the {@code SpanExporter} from where this service is
+   *                     unregistered.
+   */
+  @VisibleForTesting
+  static void unregister(SpanExporter spanExporter) {
+    spanExporter.unregisterHandler(REGISTER_NAME);
+  }
+}
---
+++ exporters/trace_zipkin/src/test/java/io/opencensus/exporter/trace/zipkin/ZipkinExporterHandlerTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2017, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.exporter.trace.zipkin;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.BaseEncoding;
+import io.opencensus.common.Timestamp;
+import io.opencensus.trace.Annotation;
+import io.opencensus.trace.AttributeValue;
+import io.opencensus.trace.Link;
+import io.opencensus.trace.NetworkEvent;
+import io.opencensus.trace.NetworkEvent.Type;
+import io.opencensus.trace.SpanContext;
+import io.opencensus.trace.SpanId;
+import io.opencensus.trace.Status;
+import io.opencensus.trace.TraceId;
+import io.opencensus.trace.TraceOptions;
+import io.opencensus.trace.export.SpanData;
+import io.opencensus.trace.export.SpanData.Attributes;
+import io.opencensus.trace.export.SpanData.Links;
+import io.opencensus.trace.export.SpanData.TimedEvent;
+import io.opencensus.trace.export.SpanData.TimedEvents;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import zipkin2.Endpoint;
+import zipkin2.Span;
+import zipkin2.Span.Kind;
+
+/**
+ * Unit tests for {@link ZipkinExporterHandler}.
+ */
+@RunWith(JUnit4.class)
+public class ZipkinExporterHandlerTest {
+
+  @Test
+  public void generateSpan() {
+    Endpoint localEndpoint = Endpoint.newBuilder().serviceName("tweetiebird").build();
+    String traceId = "d239036e7d5cec116b562147388b35bf";
+    String spanId = "9cc1e3049173be09";
+    String parentId = "8b03ab423da481c5";
+    Map<String, AttributeValue> attributes = Collections.emptyMap();
+    List<TimedEvent<Annotation>> annotations = Collections.emptyList();
+    List<TimedEvent<NetworkEvent>> networkEvents = ImmutableList.of(
+        TimedEvent.create(
+            Timestamp.create(1505855799, 433901068),
+            NetworkEvent.builder(Type.RECV, 0).setCompressedMessageSize(7).build()
+        ),
+        TimedEvent.create(
+            Timestamp.create(1505855799, 459486280),
+            NetworkEvent.builder(Type.SENT, 0).setCompressedMessageSize(13).build()
+        )
+    );
+    SpanData data = SpanData.create(
+        SpanContext.create(
+            // TODO SpanId.fromLowerBase16
+            TraceId.fromBytes(BaseEncoding.base16().lowerCase().decode(traceId)),
+            // TODO SpanId.fromLowerBase16
+            SpanId.fromBytes(BaseEncoding.base16().lowerCase().decode(spanId)),
+            TraceOptions.fromBytes(new byte[]{1} /* sampled */)
+        ),
+        // TODO SpanId.fromLowerBase16
+        SpanId.fromBytes(BaseEncoding.base16().lowerCase().decode(parentId)),
+        true, /* hasRemoteParent */
+        "Recv.helloworld.Greeter.SayHello", /* name */
+        Timestamp.create(1505855794, 194009601) /* startTimestamp */,
+        Attributes.create(attributes, 0 /* droppedAttributesCount */),
+        TimedEvents.create(annotations, 0 /* droppedEventsCount */),
+        TimedEvents.create(networkEvents, 0 /* droppedEventsCount */),
+        Links.create(Collections.<Link>emptyList(), 0 /* droppedLinksCount */),
+        null, /* childSpanCount */
+        Status.OK,
+        Timestamp.create(1505855799, 465726528) /* endTimestamp */
+    );
+
+    assertThat(ZipkinExporterHandler.generateSpan(data, localEndpoint)).isEqualTo(Span.newBuilder()
+        .traceId(traceId)
+        .parentId(parentId)
+        .id(spanId)
+        .kind(Kind.SERVER)
+        .name(data.getName())
+        .timestamp(1505855794000000L + 194009601L / 1000)
+        .duration((1505855799000000L + 465726528L / 1000) - (1505855794000000L + 194009601L / 1000))
+        .localEndpoint(localEndpoint)
+        .addAnnotation(1505855799000000L + 433901068L / 1000, "RECV")
+        .addAnnotation(1505855799000000L + 459486280L / 1000, "SENT")
+        .putTag("census.status_code", "OK")
+        .build()
+    );
+  }
+}
