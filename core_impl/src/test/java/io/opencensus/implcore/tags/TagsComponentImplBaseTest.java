84e506a6e3388faf57e66cf5e2c169960b3d297b
Kristen Kozak <sebright@google.com>
2017-10-05 13:41:33 -0700

    Rename TagComponentImplBaseTest to match class under test.

--- core_impl/src/test/java/io/opencensus/implcore/tags/TagsComponentImplBaseTest.java
+++ core_impl/src/test/java/io/opencensus/implcore/tags/TagsComponentImplBaseTest.java
@@ -26,7 +26,7 @@ import org.junit.runners.JUnit4;
 
 /** Tests for {@link TagsComponentImplBase}. */
 @RunWith(JUnit4.class)
-public class TagComponentImplBaseTest {
+public class TagsComponentImplBaseTest {
   private final TagsComponent tagsComponent = new TagsComponentImplBase();
 
   @Test
