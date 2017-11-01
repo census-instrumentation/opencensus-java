644e16bb28cce8c01b30c67070c9871c6db92957
Stefan Schmidt <stschmidt@google.com>
2017-11-02 00:15:53 +0100

    Use non-deprecated MockitoJUnitRunner.

--- contrib/agent/src/test/java/io/opencensus/contrib/agent/instrumentation/ThreadInstrumentationTest.java
+++ contrib/agent/src/test/java/io/opencensus/contrib/agent/instrumentation/ThreadInstrumentationTest.java
@@ -24,7 +24,7 @@ import net.bytebuddy.agent.builder.AgentBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 /**
  * Unit tests for {@link ThreadInstrumentation}.
