2b145b3d37549c37b6846fe6f3a736cb4e11c669
Stefan Schmidt <ubschmidt2@users.noreply.github.com>
2017-11-05 16:09:17 +0100

    Wrap the execution of URL#getContent in a trace span. (#780)
    * Wrap the execution of URL#getContent in a trace span.
    
    * Run on Java 6 runtime.
    
    * Mention tracing of URL#getContent in README.md.
    
    * Mention span name in Javadoc.
    
    * Test that span is also closed if there's an exception.

---
+++ contrib/agent/src/integration-test/java/io/opencensus/contrib/agent/instrumentation/UrlInstrumentationIT.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.contrib.agent.instrumentation;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.fail;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import io.opencensus.testing.export.TestHandler;
+import io.opencensus.trace.Tracing;
+import io.opencensus.trace.export.SpanData;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Integration tests for {@link UrlInstrumentation}.
+ *
+ * <p>The integration tests are executed in a separate JVM that has the OpenCensus agent enabled via
+ * the {@code -javaagent} command line option.
+ */
+@RunWith(JUnit4.class)
+@SuppressWarnings("checkstyle:AbbreviationAsWordInName")
+public class UrlInstrumentationIT {
+
+  private static final TestHandler testHandler = new TestHandler();
+
+  @BeforeClass
+  public static void beforeClass() {
+    Tracing.getExportComponent().getSpanExporter().registerHandler("test", testHandler);
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    Tracing.getExportComponent().getSpanExporter().unregisterHandler("test");
+  }
+
+  @Test(timeout = 60000)
+  public void getContent() throws Exception {
+    URL url = getClass().getResource("some_resource.txt").toURI().toURL();
+    Object content = url.getContent();
+
+    assertThat(content).isInstanceOf(InputStream.class);
+    assertThat(CharStreams.toString(new InputStreamReader((InputStream) content, Charsets.UTF_8)))
+        .isEqualTo("Some resource.");
+
+    SpanData span = testHandler.waitForExport(1).get(0);
+    assertThat(span.getName()).isEqualTo("java.net.URL#getContent");
+    assertThat(span.getStatus().isOk()).isTrue();
+  }
+
+  @Test(timeout = 60000)
+  public void getContent_fails() throws MalformedURLException {
+    URL url = new URL("file:///nonexistent");
+
+    try {
+      url.getContent();
+      fail();
+    } catch (IOException e) {
+      SpanData span = testHandler.waitForExport(1).get(0);
+      assertThat(span.getName()).isEqualTo("java.net.URL#getContent");
+      assertThat(span.getStatus().isOk()).isTrue();
+    }
+  }
+}
---
+++ contrib/agent/src/main/java/io/opencensus/contrib/agent/instrumentation/UrlInstrumentation.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2017, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.contrib.agent.instrumentation;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+import com.google.auto.service.AutoService;
+import com.google.errorprone.annotations.MustBeClosed;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.opencensus.contrib.agent.bootstrap.TraceTrampoline;
+import java.io.Closeable;
+import java.io.IOException;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.utility.JavaModule;
+
+/**
+ * Wraps the execution of {@link java.net.URL#getContent()} in a trace span.
+ *
+ * <p>TODO(stschmidt): Replace this preliminary, java.net.URL-specific implementation with a
+ * generic, configurable implementation.
+ */
+@AutoService(Instrumenter.class)
+public final class UrlInstrumentation implements Instrumenter {
+
+  @Override
+  public AgentBuilder instrument(AgentBuilder agentBuilder) {
+    checkNotNull(agentBuilder, "agentBuilder");
+
+    return agentBuilder.type(named("java.net.URL")).transform(new Transformer());
+  }
+
+  private static class Transformer implements AgentBuilder.Transformer {
+
+    @Override
+    public DynamicType.Builder<?> transform(
+        DynamicType.Builder<?> builder,
+        TypeDescription typeDescription,
+        ClassLoader classLoader,
+        JavaModule module) {
+      return builder.visit(Advice.to(GetContent.class).on(named("getContent")));
+    }
+  }
+
+  private static class GetContent {
+
+    /**
+     * Starts a new span and sets it as the current span when entering the method.
+     *
+     * <p>The name of the new span is constructed from the name of the instrumented class and
+     * method. For example, in case of {@link java.net.URL#getContent()} the span name is {@code
+     * java.net.URL#getContent}.
+     *
+     * <p>NB: This method is never called as is. Instead, Byte Buddy copies the method's bytecode
+     * into Executor#execute.
+     *
+     * @see Advice
+     */
+    @Advice.OnMethodEnter
+    @SuppressFBWarnings("UPM_UNCALLED_PRIVATE_METHOD")
+    @MustBeClosed
+    private static Closeable enter(@Advice.Origin("#t\\##m") String classAndMethodName) {
+      return TraceTrampoline.startScopedSpan(classAndMethodName);
+    }
+
+    /**
+     * Closes the current span when exiting the method.
+     *
+     * <p>NB: This method is never called as is. Instead, Byte Buddy copies the method's bytecode
+     * into Executor#execute.
+     *
+     * <p>NB: By default, any {@link Throwable} thrown during the advice's execution is silently
+     * suppressed.
+     *
+     * @see Advice
+     */
+    @Advice.OnMethodExit(onThrowable = Throwable.class)
+    @SuppressFBWarnings("UPM_UNCALLED_PRIVATE_METHOD")
+    private static void exit(@Advice.Enter Closeable scopedSpanHandle) throws IOException {
+      scopedSpanHandle.close();
+    }
+  }
+}
