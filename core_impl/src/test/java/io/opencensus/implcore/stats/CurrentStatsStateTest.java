01b893ac2b92e605829eef21c26c076afd537a10
songy23 <songya@google.com>
2017-11-07 13:11:28 -0800

    Add CurrentStatsState

---
+++ core_impl/src/test/java/io/opencensus/implcore/stats/CurrentStatsStateTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.implcore.stats;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import io.opencensus.stats.StatsCollectionState;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link CurrentStatsState}. */
+@RunWith(JUnit4.class)
+public final class CurrentStatsStateTest {
+
+  @Rule public final ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void defaultState() {
+    assertThat(new CurrentStatsState().get()).isEqualTo(StatsCollectionState.ENABLED);
+  }
+
+  @Test
+  public void setState() {
+    CurrentStatsState state = new CurrentStatsState();
+    state.set(StatsCollectionState.DISABLED);
+    assertThat(state.get()).isEqualTo(StatsCollectionState.DISABLED);
+    state.set(StatsCollectionState.ENABLED);
+    assertThat(state.get()).isEqualTo(StatsCollectionState.ENABLED);
+  }
+
+  @Test
+  public void preventNull() {
+    CurrentStatsState state = new CurrentStatsState();
+    thrown.expect(NullPointerException.class);
+    thrown.expectMessage("state");
+    state.set(null);
+  }
+}
