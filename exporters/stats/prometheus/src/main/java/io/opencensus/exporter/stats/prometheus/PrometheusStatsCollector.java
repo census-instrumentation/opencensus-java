7f3093e92c1135bf059143f11dbb95d9d7b36512
songy23 <songya@google.com>
2018-02-13 14:09:22 -0800

    Instrument the Prometheus Stats Exporter with trace

--- exporters/stats/prometheus/src/main/java/io/opencensus/exporter/stats/prometheus/PrometheusStatsCollector.java
+++ exporters/stats/prometheus/src/main/java/io/opencensus/exporter/stats/prometheus/PrometheusStatsCollector.java
@@ -17,11 +17,17 @@
 package io.opencensus.exporter.stats.prometheus;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import io.opencensus.common.Scope;
 import io.opencensus.stats.Stats;
 import io.opencensus.stats.View;
 import io.opencensus.stats.ViewData;
 import io.opencensus.stats.ViewManager;
+import io.opencensus.trace.Span;
+import io.opencensus.trace.Status;
+import io.opencensus.trace.Tracer;
+import io.opencensus.trace.Tracing;
 import io.prometheus.client.Collector;
 import io.prometheus.client.CollectorRegistry;
 import java.util.List;
@@ -36,6 +42,7 @@ import java.util.logging.Logger;
 public final class PrometheusStatsCollector extends Collector implements Collector.Describable {
 
   private static final Logger logger = Logger.getLogger(PrometheusStatsCollector.class.getName());
+  private static final Tracer tracer = Tracing.getTracer();
 
   private final ViewManager viewManager;
 
@@ -54,7 +61,9 @@ public final class PrometheusStatsCollector extends Collector implements Collect
   @Override
   public List<MetricFamilySamples> collect() {
     List<MetricFamilySamples> samples = Lists.newArrayList();
-    try {
+    Span span = tracer.spanBuilder("ExportStatsToPrometheus").setRecordEvents(true).startSpan();
+    span.addAnnotation("Collect Prometheus Metric Samples.");
+    try (Scope scope = tracer.withSpan(span)) {
       for (View view : viewManager.getAllExportedViews()) {
         ViewData viewData = viewManager.getView(view.getName());
         if (viewData == null) {
@@ -63,27 +72,49 @@ public final class PrometheusStatsCollector extends Collector implements Collect
           samples.add(PrometheusExportUtils.createMetricFamilySamples(viewData));
         }
       }
+      span.addAnnotation("Finish collecting Prometheus Metric Samples.");
     } catch (Throwable e) {
       logger.log(Level.WARNING, "Exception thrown when collecting metric samples.", e);
+      span.setStatus(
+          Status.UNKNOWN.withDescription(
+              "Exception thrown when collecting Prometheus Metric Samples: "
+                  + exceptionMessage(e)));
     }
+    span.end();
     return samples;
   }
 
   @Override
   public List<MetricFamilySamples> describe() {
     List<MetricFamilySamples> samples = Lists.newArrayList();
-    try {
+    Span span =
+        tracer.spanBuilder("DescribeMetricsForPrometheus").setRecordEvents(true).startSpan();
+    span.addAnnotation("Describe Prometheus Metrics.");
+    try (Scope scope = tracer.withSpan(span)) {
       for (View view : viewManager.getAllExportedViews()) {
         samples.add(PrometheusExportUtils.createDescribableMetricFamilySamples(view));
       }
+      span.addAnnotation("Finish describing Prometheus Metrics.");
     } catch (Throwable e) {
       logger.log(Level.WARNING, "Exception thrown when describing metrics.", e);
+      span.setStatus(
+          Status.UNKNOWN.withDescription(
+              "Exception thrown when describing Prometheus Metrics: " + exceptionMessage(e)));
     }
+    span.end();
     return samples;
   }
 
   @VisibleForTesting
   PrometheusStatsCollector(ViewManager viewManager) {
     this.viewManager = viewManager;
+    Tracing.getExportComponent()
+        .getSampledSpanStore()
+        .registerSpanNamesForCollection(
+            ImmutableList.of("DescribeMetricsForPrometheus", "ExportStatsToPrometheus"));
+  }
+
+  private static String exceptionMessage(Throwable e) {
+    return e.getMessage() != null ? e.getMessage() : e.getClass().getName();
   }
 }
