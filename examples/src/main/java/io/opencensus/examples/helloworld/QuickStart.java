d9325ed43be6db29d9a1eede8c24c9b3baecacc3
songy23 <songya@google.com>
2018-03-01 11:59:20 -0800

    Add javadoc for main

--- examples/src/main/java/io/opencensus/examples/helloworld/QuickStart.java
+++ examples/src/main/java/io/opencensus/examples/helloworld/QuickStart.java
@@ -69,7 +69,7 @@ public final class QuickStart {
       Collections.singletonList(FRONTEND_KEY),
       Cumulative.create());
 
-
+  /** Main launcher for the QuickStart example. */
   public static void main(String[] args) {
     TagContextBuilder tagContextBuilder =
         tagger.currentBuilder().put(FRONTEND_KEY, TagValue.create("mobile-ios9.3.5"));
