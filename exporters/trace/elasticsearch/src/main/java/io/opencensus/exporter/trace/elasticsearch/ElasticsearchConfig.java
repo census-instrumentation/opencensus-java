2435677d95867cceae5fab6decd40fa3ae0dacda
malike <st.malike@gmail.com>
2018-03-05 00:18:24 +0000

    elasticsearch trace exporter setup

---
+++ exporters/trace/elasticsearch/src/main/java/io/opencensus/exporter/trace/elasticsearch/ElasticsearchConfig.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.exporter.trace.elasticsearch;
+
+/**
+ * @autor malike_st
+ */
+public class ElasticsearchConfig {
+
+
+    private final String userName;
+    private final String password;
+    private final String elasticsearchUrl;
+    private final String elasticsearchIndex;
+
+    public ElasticsearchConfig(String userName,
+        String password, String elasticsearchUrl, String elasticsearchIndex) {
+        this.userName = userName;
+        this.password = password;
+        this.elasticsearchUrl = elasticsearchUrl;
+        this.elasticsearchIndex = elasticsearchIndex;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getElasticsearchUrl() {
+        return elasticsearchUrl;
+    }
+
+    public String getElasticsearchIndex() {
+        return elasticsearchIndex;
+    }
+}
--- exporters/trace/elasticsearch/src/main/java/io/opencensus/exporter/trace/elasticsearch/ElasticsearchTraceExporter.java
+++ exporters/trace/elasticsearch/src/main/java/io/opencensus/exporter/trace/elasticsearch/ElasticsearchTraceExporter.java
@@ -1,7 +1,62 @@
+/*
+ * Copyright 2018, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.opencensus.exporter.trace.elasticsearch;
+
+import io.opencensus.trace.export.SpanData;
+import io.opencensus.trace.export.SpanExporter;
+
+import java.io.IOException;
+import java.util.Collection;
 
 /**
  * @autor malike_st
  */
 public class ElasticsearchTraceExporter {
+
+    private static final String REGISTER_NAME = ElasticsearchTraceExporter.class.getName();
+
+
+    public static void createAndRegister(ElasticsearchConfig elasticsearchConfig)
+            throws IOException {
+
+    }
+
+    static void register(SpanExporter spanExporter, SpanExporter.Handler handler) {
+
+    }
+
+    public static void unregister() {
+
+    }
+
+    public static void unregister(SpanExporter spanExporter) {
+        spanExporter.unregisterHandler(REGISTER_NAME);
+    }
+
+
+    static final class ElasticsearchTraceHandler extends SpanExporter.Handler {
+
+        @Override
+        public void export(Collection<SpanData> spanDataList) {
+            for (SpanData spanData : spanDataList) {
+             //log to elasticsearch
+            }
+        }
+
+    }
+
 }
