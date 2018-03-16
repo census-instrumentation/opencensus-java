c0d20639e608dc67a691648856c26e2923e79c15
Yang Song <songy23@users.noreply.github.com>
2018-03-15 20:34:14 -0700

    Fix a failing test (#1063)

--- exporters/trace/jaeger/src/test/java/io/opencensus/exporter/trace/jaeger/JaegerExporterHandlerIntegrationTest.java
+++ exporters/trace/jaeger/src/test/java/io/opencensus/exporter/trace/jaeger/JaegerExporterHandlerIntegrationTest.java
@@ -144,7 +144,7 @@ public class JaegerExporterHandlerIntegrationTest {
       final JsonObject span = spans.get(0).getAsJsonObject();
       assertThat(span, is(not(nullValue())));
       assertThat(span.get("traceID").getAsString(), matchesPattern("[a-z0-9]{31,32}"));
-      assertThat(span.get("spanID").getAsString(), matchesPattern("[a-z0-9]{16}"));
+      assertThat(span.get("spanID").getAsString(), matchesPattern("[a-z0-9]{15,16}"));
       assertThat(span.get("flags").getAsInt(), is(1));
       assertThat(span.get("operationName").getAsString(), is(SPAN_NAME));
       assertThat(span.get("references").getAsJsonArray().size(), is(0));
