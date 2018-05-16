9d4cb2769911cd208d095ce78befd325047c1251
Kristen Kozak <sebright@google.com>
2018-05-15 17:29:46 -0700

    Avoid sending negative timestamps to Stackdriver.

--- exporters/stats/stackdriver/src/test/java/io/opencensus/exporter/stats/stackdriver/StackdriverExportUtilsTest.java
+++ exporters/stats/stackdriver/src/test/java/io/opencensus/exporter/stats/stackdriver/StackdriverExportUtilsTest.java
@@ -226,10 +226,12 @@ public class StackdriverExportUtilsTest {
     assertThat(StackdriverExportUtils.convertTimestamp(censusTimestamp1))
         .isEqualTo(
             com.google.protobuf.Timestamp.newBuilder().setSeconds(100).setNanos(3000).build());
+
+    // Stackdriver doesn't allow negative values, instead it will replace the negative values
+    // by returning a default instance.
     Timestamp censusTimestamp2 = Timestamp.create(-100, 3000);
     assertThat(StackdriverExportUtils.convertTimestamp(censusTimestamp2))
-        .isEqualTo(
-            com.google.protobuf.Timestamp.newBuilder().setSeconds(-100).setNanos(3000).build());
+        .isEqualTo(com.google.protobuf.Timestamp.newBuilder().build());
   }
 
   @Test
