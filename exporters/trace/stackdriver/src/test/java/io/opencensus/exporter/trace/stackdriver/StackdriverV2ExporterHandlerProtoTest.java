a5fa04df965e9e59f37e9f99229ebc88c2dc7431
songy23 <songya@google.com>
2018-06-13 14:24:55 -0700

    Add missing Javadoc for unit tests.

--- exporters/trace/stackdriver/src/test/java/io/opencensus/exporter/trace/stackdriver/StackdriverV2ExporterHandlerProtoTest.java
+++ exporters/trace/stackdriver/src/test/java/io/opencensus/exporter/trace/stackdriver/StackdriverV2ExporterHandlerProtoTest.java
@@ -62,6 +62,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+/** Unit tests for proto conversions in {@link StackdriverV2ExporterHandler}. */
 @RunWith(JUnit4.class)
 public final class StackdriverV2ExporterHandlerProtoTest {
 
