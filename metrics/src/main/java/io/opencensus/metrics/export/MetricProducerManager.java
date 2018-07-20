0a9d182e4cf43f4112c933a5aff0ae052de08f5f
Bogdan Drutu <bdrutu@google.com>
2018-07-20 11:27:24 -0400

    Add metric components for export and main package and metric producer. (#1322)
    * Add metric components for export and main package and metric producer.
    
    * Solve first round of comments.
    
    * Minor comments fixes.

---
+++ metrics/src/main/java/io/opencensus/metrics/export/MetricProducerManager.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2018, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.metrics.export;
+
+import io.opencensus.common.ExperimentalApi;
+import io.opencensus.internal.Utils;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * Keeps a set of {@link MetricProducer} that is used by exporters to determine the metrics that
+ * need to be exported.
+ *
+ * @since 0.16
+ */
+@ExperimentalApi
+@ThreadSafe
+public final class MetricProducerManager {
+  private volatile Set<MetricProducer> metricProducers =
+      Collections.unmodifiableSet(new LinkedHashSet<MetricProducer>());
+
+  /**
+   * Adds the {@link MetricProducer} to the manager if it is not already present.
+   *
+   * @param metricProducer the {@code MetricProducer} to be added to the manager.
+   * @since 0.16
+   */
+  public synchronized void add(MetricProducer metricProducer) {
+    Utils.checkNotNull(metricProducer, "metricProducer");
+    // Updating the set of MetricProducers happens under a lock to avoid multiple add or remove
+    // operations to happen in the same time.
+    Set<MetricProducer> newMetricProducers = new LinkedHashSet<MetricProducer>(metricProducers);
+    if (!newMetricProducers.add(metricProducer)) {
+      // The element already present, no need to update the current set of MetricProducers.
+      return;
+    }
+    metricProducers = Collections.unmodifiableSet(newMetricProducers);
+  }
+
+  /**
+   * Removes the {@link MetricProducer} to the manager if it is present.
+   *
+   * @param metricProducer the {@code MetricProducer} to be removed from the manager.
+   */
+  public synchronized void remove(MetricProducer metricProducer) {
+    Utils.checkNotNull(metricProducer, "metricProducer");
+    // Updating the set of MetricProducers happens under a lock to avoid multiple add or remove
+    // operations to happen in the same time.
+    Set<MetricProducer> newMetricProducers = new LinkedHashSet<MetricProducer>(metricProducers);
+    if (!newMetricProducers.remove(metricProducer)) {
+      // The element not present, no need to update the current set of MetricProducers.
+      return;
+    }
+    metricProducers = Collections.unmodifiableSet(newMetricProducers);
+  }
+
+  /**
+   * Returns all registered {@link MetricProducer}s that should be exported.
+   *
+   * <p>This method should be used by any metrics exporter that automatically exports data for
+   * {@code MetricProducer} registered with the {@code MetricProducerManager}.
+   *
+   * @return all registered {@code MetricProducer}s that should be exported.
+   */
+  public Set<MetricProducer> getAllMetricProducer() {
+    return metricProducers;
+  }
+
+  // Package protected to allow us to possibly change this to an abstract class in the future. This
+  // ensures that nobody can create an instance of this class except ExportComponent.
+  MetricProducerManager() {}
+}
---
+++ metrics/src/test/java/io/opencensus/metrics/export/MetricProducerManagerTest.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2018, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.metrics.export;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+/** Unit tests for {@link MetricProducerManager}. */
+@RunWith(JUnit4.class)
+public class MetricProducerManagerTest {
+  private final MetricProducerManager metricProducerManager = new MetricProducerManager();
+  @Mock private MetricProducer metricProducer;
+  @Mock private MetricProducer metricProducerOther;
+
+  @Rule public final ExpectedException thrown = ExpectedException.none();
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void add_DisallowsNull() {
+    thrown.expect(NullPointerException.class);
+    metricProducerManager.add(null);
+  }
+
+  @Test
+  public void add() {
+    metricProducerManager.add(metricProducer);
+    assertThat(metricProducerManager.getAllMetricProducer()).containsExactly(metricProducer);
+  }
+
+  @Test
+  public void add_DuplicateElement() {
+    metricProducerManager.add(metricProducer);
+    Set<MetricProducer> metricProducerSet = metricProducerManager.getAllMetricProducer();
+    assertThat(metricProducerSet).containsExactly(metricProducer);
+    metricProducerManager.add(metricProducer);
+    // Returns the same object.
+    assertThat(metricProducerManager.getAllMetricProducer()).isSameAs(metricProducerSet);
+  }
+
+  @Test
+  public void add_MultipleElements() {
+    metricProducerManager.add(metricProducer);
+    Set<MetricProducer> metricProducerSet = metricProducerManager.getAllMetricProducer();
+    assertThat(metricProducerSet).containsExactly(metricProducer);
+    metricProducerManager.add(metricProducerOther);
+    // Returns the same object.
+    assertThat(metricProducerManager.getAllMetricProducer())
+        .containsExactly(metricProducer, metricProducerOther);
+  }
+
+  @Test
+  public void addAndRemove() {
+    metricProducerManager.add(metricProducer);
+    assertThat(metricProducerManager.getAllMetricProducer()).containsExactly(metricProducer);
+    metricProducerManager.remove(metricProducer);
+    assertThat(metricProducerManager.getAllMetricProducer()).isEmpty();
+  }
+
+  @Test
+  public void remove_DisallowsNull() {
+    thrown.expect(NullPointerException.class);
+    metricProducerManager.remove(null);
+  }
+
+  @Test
+  public void remove_FromEmpty() {
+    metricProducerManager.remove(metricProducer);
+    assertThat(metricProducerManager.getAllMetricProducer()).isEmpty();
+  }
+
+  @Test
+  public void remove_NotPresent() {
+    metricProducerManager.add(metricProducer);
+    Set<MetricProducer> metricProducerSet = metricProducerManager.getAllMetricProducer();
+    assertThat(metricProducerSet).containsExactly(metricProducer);
+    metricProducerManager.remove(metricProducerOther);
+    // Returns the same object.
+    assertThat(metricProducerManager.getAllMetricProducer()).isSameAs(metricProducerSet);
+  }
+
+  @Test
+  public void getAllMetricProducer_empty() {
+    assertThat(metricProducerManager.getAllMetricProducer()).isEmpty();
+  }
+}
