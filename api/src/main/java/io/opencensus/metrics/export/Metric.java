c705a0156961f9280f4ae9060b646d9767a6b4ee
songy23 <songya@google.com>
2018-10-09 17:59:13 -0700

    Check for null and add more tests.

--- api/src/main/java/io/opencensus/metrics/export/Metric.java
+++ api/src/main/java/io/opencensus/metrics/export/Metric.java
@@ -49,6 +49,9 @@ public abstract class Metric {
    * @since 0.17
    */
   public static Metric create(MetricDescriptor metricDescriptor, List<TimeSeries> timeSeriesList) {
+    Utils.checkNotNull(metricDescriptor, "metricDescriptor");
+    Utils.checkNotNull(timeSeriesList, "timeSeriesList");
+    Utils.checkListElementNotNull(timeSeriesList, "timeSeries");
     checkTypeMatch(metricDescriptor.getType(), timeSeriesList);
     return new AutoValue_Metric(
         metricDescriptor, Collections.unmodifiableList(new ArrayList<TimeSeries>(timeSeriesList)));
