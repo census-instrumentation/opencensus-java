8513a5e1deaa6e0123b79220db83c840c82f9259
Mayur Kale <mayurkale@google.com>
2018-11-02 11:10:38 -0700

    Add resource package

---
+++ api/src/main/java/io/opencensus/resource/Resource.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2018, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.resource;
+
+import com.google.auto.value.AutoValue;
+import io.opencensus.common.ExperimentalApi;
+import io.opencensus.internal.DefaultVisibilityForTesting;
+import io.opencensus.internal.StringUtils;
+import io.opencensus.internal.Utils;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.Immutable;
+
+/**
+ * {@link Resource} represents a resource, which capture identifying information about the entities
+ * for which signals (stats or traces) are reported. It further provides a framework for detection
+ * of resource information from the environment and progressive population as signals propagate from
+ * the core instrumentation library to a backend's exporter.
+ *
+ * @since 0.18
+ */
+@Immutable
+@AutoValue
+@ExperimentalApi
+public abstract class Resource {
+  @DefaultVisibilityForTesting static final int MAX_LENGTH = 255;
+  private static final String OC_RESOURCE_TYPE_ENV = "OC_RESOURCE_TYPE";
+  private static final String OC_RESOURCE_LABELS_ENV = "OC_RESOURCE_LABELS";
+  private static final Pattern LABEL_REGEX =
+      Pattern.compile("\\s*([a-zA-Z0-9-_./]+)=(?:(\".*?\")|([^\\s=\"]+))\\s*,");
+  private static final String ERROR_MESSAGE_INVALID_CHARS =
+      " should be a ASCII string with a length no greater than " + MAX_LENGTH + " characters.";
+
+  @Nullable
+  private static final String ENV_TYPE = parseResourceType(System.getenv(OC_RESOURCE_TYPE_ENV));
+
+  private static final Map<String, String> ENV_LABEL_MAP =
+      parseResourceLabels(System.getenv(OC_RESOURCE_LABELS_ENV));
+
+  Resource() {}
+
+  /**
+   * Returns the type identifier for the resource.
+   *
+   * @return the type identifier for the resource.
+   * @since 0.18
+   */
+  @Nullable
+  public abstract String getType();
+
+  /**
+   * Returns a map of labels that describe the resource.
+   *
+   * @return a map of labels.
+   * @since 0.18
+   */
+  public abstract Map<String, String> getLabels();
+
+  /**
+   * Returns a {@link Resource}. This resource information is loaded from the OC_RESOURCE_TYPE and
+   * OC_RESOURCE_LABELS environment variables.
+   *
+   * @return a {@code Resource}.
+   * @since 0.18
+   */
+  public static Resource create() {
+    return create(ENV_TYPE, ENV_LABEL_MAP);
+  }
+
+  /**
+   * Returns a {@link Resource}.
+   *
+   * @param type the type identifier for the resource.
+   * @param labels a map of labels that describe the resource.
+   * @return a {@code Resource}.
+   * @throws NullPointerException if {@code labels} is null.
+   * @throws IllegalArgumentException if type or label key or label value is not a valid printable
+   *     ASCII string or exceed {@link #MAX_LENGTH} characters.
+   * @since 0.18
+   */
+  public static Resource create(@Nullable String type, Map<String, String> labels) {
+    Utils.checkNotNull(labels, "labels");
+    return new AutoValue_Resource(type, labels);
+  }
+
+  /**
+   * Creates a resource type from the OC_RESOURCE_TYPE environment variable.
+   *
+   * <p>OC_RESOURCE_TYPE: A string that describes the type of the resource prefixed by a domain
+   * namespace, e.g. “kubernetes.io/container”.
+   */
+  @Nullable
+  static String parseResourceType(@Nullable String rawEnvType) {
+    if (rawEnvType != null && !rawEnvType.isEmpty()) {
+      Utils.checkArgument(isValid(rawEnvType), "Type" + ERROR_MESSAGE_INVALID_CHARS);
+      return rawEnvType.trim();
+    }
+    return rawEnvType;
+  }
+
+  /*
+   * Creates a label map from the OC_RESOURCE_LABELS environment variable.
+   *
+   * <p>OC_RESOURCE_LABELS: A comma-separated list of labels describing the source in more detail,
+   * e.g. “key1=val1,key2=val2”. Domain names and paths are accepted as label keys. Values may be
+   * quoted or unquoted in general. If a value contains whitespaces, =, or " characters, it must
+   * always be quoted.
+   */
+  static Map<String, String> parseResourceLabels(@Nullable String rawEnvLabels) {
+    if (rawEnvLabels == null) {
+      return Collections.<String, String>emptyMap();
+    } else {
+      Map<String, String> labels = new HashMap<String, String>();
+      String envLabels = rawEnvLabels.trim();
+      // Ensure a trailing comma, which allows us to keep the regex simpler
+      if (envLabels.charAt(envLabels.length() - 1) != ',') {
+        envLabels = envLabels.trim() + ',';
+      }
+
+      Matcher matcher = LABEL_REGEX.matcher(envLabels);
+      while (matcher.find()) {
+        String match = matcher.group(0);
+        if (match != null && match.length() > 0) {
+          String key = matcher.group(1);
+          String value = matcher.group(2);
+
+          value = value == null ? matcher.group(3) : value.replaceAll("^\"|\"$", "");
+          if (key != null && value != null) {
+            Utils.checkArgument(isValid(key), "Label key" + ERROR_MESSAGE_INVALID_CHARS);
+            Utils.checkArgument(isValid(value), "Label value" + ERROR_MESSAGE_INVALID_CHARS);
+            labels.put(key, value);
+          }
+        }
+      }
+      return labels;
+    }
+  }
+
+  /**
+   * Determines whether the given {@code String} is a valid printable ASCII string.
+   *
+   * @param name the name to be validated.
+   * @return whether the name is valid.
+   */
+  static boolean isValid(String name) {
+    return !name.isEmpty() && name.length() <= MAX_LENGTH && StringUtils.isPrintableString(name);
+  }
+
+  // TODO(mayurkale): Add detector interface as per
+  // https://github.com/census-instrumentation/opencensus-specs/blob/master/resource/Resource.md.
+}
