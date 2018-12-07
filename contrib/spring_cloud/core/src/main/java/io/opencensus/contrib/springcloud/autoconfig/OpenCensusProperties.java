711fc5d6f725292d7462ddbaa67e53798ec016bb
rahulpa <rahulpa@google.com>
2018-12-07 13:30:55 -0800

    add support for opencensus http servlet plugin in spring cloud framework

---
+++ contrib/spring_cloud/core/src/main/java/io/opencensus/contrib/springcloud/autoconfig/OpenCensusProperties.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019, OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opencensus.contrib.springcloud.autoconfig;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Opencensus Tracing settings.
+ *
+ * @since 0.19.0
+ */
+@ConfigurationProperties("spring.opencensus")
+public class OpenCensusProperties {
+
+  private boolean enabled = true;
+
+  public boolean isEnabled() {
+    return this.enabled;
+  }
+
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+}
