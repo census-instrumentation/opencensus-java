8af85434c0a1542f27c9acaf4f133a9a10727ab2
Mayur Kale <mayurkale@google.com>
2020-03-26 16:43:22 -0700

    Update release versions for all readme and build files. (#2028)

--- examples/src/main/java/io/opencensus/examples/grpc/helloworld/HelloWorldClient.java
+++ examples/src/main/java/io/opencensus/examples/grpc/helloworld/HelloWorldClient.java
@@ -57,7 +57,7 @@ public class HelloWorldClient {
         ManagedChannelBuilder.forAddress(host, port)
             // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
             // needing certificates.
-            .usePlaintext(true)
+            .usePlaintext()
             .build());
   }
 
